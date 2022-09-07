9ad0d320dbab3cf16a634acd2288eb73bae865bb
utnesz <90829509+utnesz@users.noreply.github.com>
2022-09-07 21:19:50 +0200

    Not working...

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,29 @@
+// 1. Írj egy függvényt, `convertToUppercase()` néven, mely paraméterként kap egy tömböt.A függvény visszatérési érték egy Promise.  
+// Amennyiben a tömb minden eleme string, és sikeresen nagybetűssé alakíthatók a Promise resolved lesz és visszaad egy új tömböt, 
+// ami az eredeti tömb összes elemét nagybetűssé alakítva tartalmazza.  
+// Ellenkező esetben a Promise rejected lesz, a visszatérési érték a következő string: 'Error: Not all items in the array are strings!'
+
+
+// const convertToUppercase = new Promise((resolve, reject) => {
+//         let array = [];
+//         resolve(array.map(item=>item.toUpperCase))
+//         reject('Error: Not all items in the array are strings!')
+// })
+
+// convertToUppercase.then();
+
+// export default convertToUppercase;
+
+
+const convertToUppercase = (array) => {
+        return new Promise((resolve, reject) => {
+                if( !array.some(item => typeof item !== 'string')) {
+                    const modArray = array.map(item => item.toUpperCase());
+                        resolve(modArray);
+                } else {
+                        reject("Error: Error: Not all items in the array are strings!");
+                }
+                });
+        };
+
 export default convertToUppercase;
--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,23 @@
+// 2. Írj egy másik függvényt `sortStrings()` néven, mely paraméterként egy tömböt kap.A függvény visszatérési érték egy Promise.  
+// A Promise resolved lesz, amennyiben a tömb minden eleme string, a resolved value az ABC szerinti rendezett tömb.  
+// Ha nem minden elem string, akkor a Promise rejected lesz, az Error objektum üzenet szövege:
+// 'Error: Not all items in the array are strings!'   
+// Ha a sorba rendezés során hiba történik akkor is rejected lesz a Promise, az Error objektum üzenet szövege ebben az esetben:
+// 'Error: Something went wrong with sorting words.'
+
+const sortStrings = (array = ['apa', 'cuka']) => {
+    return new Promise((resolve, reject) => {
+        if (!array.some(item => typeof item !== 'string')) {
+            const modArray = array.sort();
+            resolve(modArray);
+        } else {
+            reject(["Error: Not all items in the array are strings"]);
+        }
+    });
+};
+
+newPro.then(data).catch(() => new Error = ('Error: Something went wrong with sorting words.'))
+
 export default sortStrings;
+
+
